a24ef66e811bc73eaddb4bdd361255193f8e4d5d
Martin Hnátek <m.hnatma@gmail.com>
2017-05-23 11:07:19 +0200

    Přidána integrace H2 databáze

--- build.sbt
+++ build.sbt
@@ -3,4 +3,8 @@ name := "jdf2gtfs"
 version := "1.0"
 
 scalaVersion := "2.12.2"
-        
+
+libraryDependencies ++= Seq(
+  "com.h2database" % "h2" % "1.4.195",
+  "org.scalikejdbc" %% "scalikejdbc" % "3.0.0",
+  "ch.qos.logback"  %  "logback-classic"   % "1.2.3")
--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,3 +1,12 @@
+import scalikejdbc._
+
+
 object App {
-  def main(args: Array[String]): Unit = println("Hello, world!")
+  // Inicializace databáze
+  Class.forName("org.h2.Driver")
+  ConnectionPool.singleton("jdbc:h2:mem:db1", "user", "pass")
+  implicit val session = AutoSession
+
+  def main(args: Array[String]): Unit = {
+  }
 }
